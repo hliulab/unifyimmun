51fc7b39ef0929f685e91db958b9ed48c237cd04
hliulab <105711092+hliulab@users.noreply.github.com>
2024-04-26 22:42:02 +0800

    release

---
+++ source/run_all_phases.py
@@ -0,0 +1,9 @@
+import subprocess
+
+subprocess.run(["python", "step-1-HLA_1.py"])
+
+subprocess.run(["python", "step-2-TCR_1.py"])
+
+subprocess.run(["python", "step-3-HLA_2.py"])
+
+subprocess.run(["python", "step-4-TCR_2.py"])
